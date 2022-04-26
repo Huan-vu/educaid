baa9170616b3c0fc0577afe1fd6020b5536de2dc
huanvm <huanvu2905@gmail.com>
2022-04-26 22:06:26 +0700

    add passport

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
-
+  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+  
+use App\Http\Controllers\Api\AuthController;
+  
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+  
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
